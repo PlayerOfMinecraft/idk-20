06b53d58b3bc887d743aeb063515a2abf62e0fb9
PlayerOfMinecraft <96972638+PlayerOfMinecraft@users.noreply.github.com>
2022-03-22 09:57:23 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+basic.forever(function () {
+    if (input.soundLevel() < 51) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 51 && input.soundLevel() < 102) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 102 && input.soundLevel() < 153) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 153 && input.soundLevel() < 204) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
